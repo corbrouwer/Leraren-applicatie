c72b8b27e29aed7fdde4b5c641784ad9e19c5db1
erwin775 <Erwin@e049-211.dot1x.nhl.nl>
2015-04-30 12:42:14 +0200

    database finish en problems controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429105745) do
+ActiveRecord::Schema.define(version: 20150430103136) do
 
   create_table "problems", force: :cascade do |t|
     t.integer  "user_id"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150429105745) do
     t.integer  "grade"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.string   "password"
   end
 
 end
